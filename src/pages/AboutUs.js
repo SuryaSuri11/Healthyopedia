04fa787fa2ae58c8f2ec938fbf71727726572b72
Surya <suryashanmugam171@gmail.com>
2021-12-22 22:53:25 +0530

    code before merging after completing login page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,7 @@
 import React,{useEffect} from "react";
 import "./AboutUs.css";
-import Aos from "aos";
-import "aos/dist/aos.css";
 
 function AboutUs(){
-    useEffect(()=>{
-        Aos.init({duration:2000});
-    },[]);
 return(
     <div className='aboutcontainer'>
         <h1>About Page</h1>
