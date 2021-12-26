fc4cf94676c852e3d3c17d9daadb0909155cf1f4
Surya <suryashanmugam171@gmail.com>
2021-12-26 10:02:01 +0530

    basic product page done

---
+++ React_Healthyopedia/src/components/shop_page/ProductHeader.js
@@ -0,0 +1,24 @@
+import classes from './products.module.css'
+
+function ProductHeader()
+{
+  return (
+  <div className={classes.header}>
+    <h2>Shop</h2>
+
+    {/* <p>Showing all {filterctx.products[filterctx.products.length-1].length} results</p> */}
+    <div className={classes.sortdropdown}>
+    <select id="sortingdropdown">
+    <option id="defaultsort" value="1">Default sorting</option>
+    <option id="popularitysort" value="2">Sort by popularity</option>
+    <option id="ratingsort" value="3">Sort by average rating</option>
+    <option id="latestsort" value="4">Sort by latest</option>
+    <option id="lowtohighsort" value="5">Sort by price:low to high</option>
+    <option id="hightolowsort" value="6">Sort by price:high to low</option>
+    </select>
+    </div>
+  </div>
+  );
+}
+
+export default ProductHeader;
