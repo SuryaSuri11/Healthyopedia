c8e238a597345893afa43b14cf16ca4bbcd59680
Surya <suryashanmugam171@gmail.com>
2022-01-01 18:02:20 +0530

    "news api setup done"

--- Django_Healthyopedia/api/views.py
+++ Django_Healthyopedia/api/views.py
@@ -9,8 +9,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # #news 
-# from newsapi import NewsApiClient
-# from django.http import JsonResponse
+from newsapi import NewsApiClient
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -71,9 +71,9 @@ def product_update(request,pk):
   return Response(serializer.data)
 
 
-# def news(request):
-#   newsapi = NewsApiClient(api_key='64592e96d9f4454ea620d7778ad005b9')
-#   all_articles = newsapi.get_top_headlines(language='en',category='health')
-#   return JsonResponse(all_articles) 
+def news(request):
+  newsapi = NewsApiClient(api_key='64592e96d9f4454ea620d7778ad005b9')
+  all_articles = newsapi.get_top_headlines(language='en',category='health')
+  return JsonResponse(all_articles) 
   
 
